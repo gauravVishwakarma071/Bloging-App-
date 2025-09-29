50d197e68349fdbf24a6a86b177856998ae202f5
Gaurav <gauravvishwakarma071@gmail.com>
2025-09-29 14:46:10 +0530

    User response DTO.

---
+++ src/main/java/com/spring_boot/blog_app/users/dtos/UserResponse.java
@@ -0,0 +1,12 @@
+package com.spring_boot.blog_app.users.dtos;
+
+import lombok.Data;
+
+@Data
+public class UserResponse {
+    private Long id;
+    private String username;
+    private String email;
+    private String bio;
+    private String image;
+}
