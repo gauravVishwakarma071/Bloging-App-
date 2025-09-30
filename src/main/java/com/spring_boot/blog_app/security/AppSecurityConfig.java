a938944f89f572f28dcd3cbab3be2a8a3fe03fd8
Gaurav <gauravvishwakarma071@gmail.com>
2025-09-30 14:32:41 +0530

    Configration file for App security.

---
+++ src/main/java/com/spring_boot/blog_app/security/AppSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.spring_boot.blog_app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class AppSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable) // disable or configure CORS
+                .csrf(AbstractHttpConfigurer::disable) // disable CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/users", "/users/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll) // enables form login
+                .httpBasic(basic -> {}); // enables HTTP Basic auth
+
+        return http.build();
+    }
+}
