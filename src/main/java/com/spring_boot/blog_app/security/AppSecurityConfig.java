44551d60cd2d57a0eb89e22d8cf99b0c112ef01c
Gaurav <gauravvishwakarma071@gmail.com>
2025-10-02 23:40:40 +0530

    Added bean for Authentication Filter.

--- src/main/java/com/spring_boot/blog_app/security/AppSecurityConfig.java
+++ src/main/java/com/spring_boot/blog_app/security/AppSecurityConfig.java
@@ -1,15 +1,32 @@
 package com.spring_boot.blog_app.security;
 
+import com.spring_boot.blog_app.users.UsersService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@AllArgsConstructor
 public class AppSecurityConfig {
+
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+    private final JWTService jwtService;
+    private final UsersService usersService;
+
+    @Bean
+    JWTAuthenticationFilter jwtAuthenticationFilter() throws Exception{
+        return new JWTAuthenticationFilter(new JWTAuthenticationManager(jwtService,usersService));
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,6 +39,8 @@ public class AppSecurityConfig {
                 )
                 .formLogin(AbstractAuthenticationFilterConfigurer::permitAll) // enables form login
                 .httpBasic(basic -> {}); // enables HTTP Basic auth
+
+        http.addFilterBefore(jwtAuthenticationFilter, AnonymousAuthenticationFilter.class);
 
         return http.build();
     }
