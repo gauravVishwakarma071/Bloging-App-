e816ab1af77156df6ddc28860b23ff97a067f8f9
Gaurav <gauravvishwakarma071@gmail.com>
2025-10-02 23:41:36 +0530

    implements Authentication.

---
+++ src/main/java/com/spring_boot/blog_app/security/JWTAuthenticaiton.java
@@ -0,0 +1,53 @@
+package com.spring_boot.blog_app.security;
+
+import com.spring_boot.blog_app.users.UserEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+public class JWTAuthenticaiton implements Authentication {
+
+    String jwt;
+    UserEntity userEntity;
+
+    public JWTAuthenticaiton(String jwt){
+        this.jwt = jwt;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getCredentials() {
+        return jwt;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public UserEntity getPrincipal() {
+        return userEntity;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return (userEntity != null);
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}
